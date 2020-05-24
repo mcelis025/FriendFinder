f2ad88a187a9b09decc10328fefdbdd337be45e0
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-05-23 21:09:51 -0600

    added app.get

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express();
 var path = require("path");
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "app/public/home.html"));
+});
  
 app.listen(3000)

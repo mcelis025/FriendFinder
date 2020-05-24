05dd98f53d06b937e83508cfee9be3a0302b0e9b
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-05-24 01:58:14 -0600

    fixed api get

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,13 +4,27 @@ const path = require("path");
 module.exports = function (app) {
 
     // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends 
-        app.get("/api/friends", function (req, res) {
-            res.json(path.join(__dirname, "../public/survey.html"));
-        });
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
 
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
-        app.post("/api/friends", function (req, res) {
-            res.json(path.join(__dirname, "../public/home.html"));
-        });
-    
-    };
+    app.post("/api/friends", function (req, res) {
+        
+        var newReservation = req.body;
+        if (reservations.length < 5) {
+            reservations.push(newReservation);
+
+            res.json(newReservation);
+            console.log('added to reservations');
+        } else {
+            waitlist.push(newReservation);
+
+            res.json(newReservation);
+            console.log('added to waitlist');
+        }
+
+        console.log(newReservation);
+    });
+
+};
